7ea49038fa5fa91d61952a94cca784ff540591d4
Shekhar Tarare <shekhartarare@gmail.com>
2022-08-14 22:24:13 +0530

    Add project files.

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.ResourceParameters;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    //If we inherit from Controller then it will also add support for views which is not needed in our case
+    public class AuthorsController: ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentException(nameof(courseLibraryRepository));
+            _mapper = mapper ?? 
+                throw new ArgumentException(nameof(mapper));
+        }
+
+        [HttpGet()]
+        [HttpHead]
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors(
+            [FromQuery] AuthorsResourceParameters authorsResourceParameters)
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors(authorsResourceParameters);
+            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthor(Guid id)
+        {
+            var author = _courseLibraryRepository.GetAuthor(id);
+            if (author == null)
+                return NotFound();
+            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(author));
+        }
+    }
+}
